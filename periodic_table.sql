e1a5800af91153121f434b517a01630c5e3b0f68
tfogle <tyfogle@gmail.com>
2024-04-22 16:39:44 -0400

    Update periodic_table.sql

--- periodic_table.sql
+++ periodic_table.sql
@@ -76,8 +76,8 @@ ALTER TABLE public.properties OWNER TO freecodecamp;
 --
 
 CREATE TABLE public.types (
-    type character varying(255) NOT NULL,
-    type_id integer NOT NULL
+    type_id integer NOT NULL,
+    type character varying(255) NOT NULL
 );
 
 
@@ -132,11 +132,11 @@ INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 -- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.properties VALUES (5, 10.81, 2075, 4000, 2);
 INSERT INTO public.properties VALUES (1, 1.008, -259.1, -252.9, 2);
 INSERT INTO public.properties VALUES (2, 4.0026, -272.2, -269, 2);
 INSERT INTO public.properties VALUES (3, 6.94, 180.54, 1342, 1);
 INSERT INTO public.properties VALUES (4, 9.0122, 1287, 2470, 1);
+INSERT INTO public.properties VALUES (5, 10.81, 2075, 4000, 3);
 INSERT INTO public.properties VALUES (6, 12.011, 3550, 4027, 2);
 INSERT INTO public.properties VALUES (7, 14.007, -210.1, -195.8, 2);
 INSERT INTO public.properties VALUES (8, 15.999, -218, -183, 2);
@@ -148,9 +148,9 @@ INSERT INTO public.properties VALUES (10, 20.18, -248.6, -246.1, 2);
 -- Data for Name: types; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.types VALUES ('metal', 1);
-INSERT INTO public.types VALUES ('nonmetal', 2);
-INSERT INTO public.types VALUES ('metalloid', 3);
+INSERT INTO public.types VALUES (1, 'metal');
+INSERT INTO public.types VALUES (2, 'nonmetal');
+INSERT INTO public.types VALUES (3, 'metalloid');
 
 
 --
